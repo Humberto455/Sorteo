25b2d4dc85b9e9a52d118f1dcc11818322d62cc1
Humberto455 <hrasgado3892@ppsc.edu.mx>
2022-03-06 14:48:26 -0600

    App_v1

---
+++ Concurso/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Concurso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r, c;
+            string[] participantes;
+            Random aleatorio = new Random();// De esta manera podemos instanciar un numero aleatorio
+
+            Console.WriteLine("\t\tCONCURSO AL AZAR!\n");
+            Console.WriteLine("¿Cantidad de participantes?");
+            r = int.Parse(Console.ReadLine());
+
+            participantes = new string[r];
+
+            c = 1;
+
+            for (int i = 0; i < r; i++)
+            {
+                Console.WriteLine($"Ingresa el Nombre del participante: {c}");
+                participantes[i] = Console.ReadLine();
+                c++;
+            }
+
+            Console.WriteLine("\t\tLISTA DE PARTICIPANTES!\n");
+            for (int q = 0; q < r; q++)
+            {
+                Console.WriteLine(participantes[q]);
+            }
+
+            Console.WriteLine("\t\tGANADOR!\n");
+            Console.WriteLine($"El Ganador del concurso es: {participantes[aleatorio.Next(0, r)]}");
+
+            //int t;
+            //try
+            //{
+            //    Console.WriteLine("Ingresa un Numero entero");
+            //    t = int.Parse(Console.ReadLine());
+            //}
+            //catch (FormatException)// esto es una excepcion Especifica
+            //{
+            //    t = 1;
+            //    Console.WriteLine("has Ingresado un texto y no un entero ahora el numero sera 1");
+            //}
+            //catch (Exception ex)//Esto es una excepcion Generica
+            //{
+            //    Console.WriteLine(ex.Message);
+            //    t = 1;
+            //}
+
+
+        }
+    }
+}
